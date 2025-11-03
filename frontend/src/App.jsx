b5279079aaadd9fe85b60f560e27020d1fe3ddc1
Jiolashington Ben Asher <155675926+Jbenash@users.noreply.github.com>
2025-11-04 01:42:34 +0530

    Initial commit: E-commerce app with React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Product from './pages/Product.jsx'
+import Login from './pages/Login.jsx'
+import Cart from './pages/Cart.jsx'
+import Orders from './pages/Orders.jsx'
+import PlaceOrder from './pages/PlaceOrder.jsx'
+import Contact from './pages/Contact.jsx'
+import Collection from './pages/Collection.jsx' 
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] '>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/collection' element={<Collection />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
